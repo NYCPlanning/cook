c30beadf2ee4a3b44a1fb44b9a8312347e69d8fc
sptkl <caobaiyue@gmail.com>
2019-11-26 16:24:25 -0500

    update

--- cook/importer.py
+++ cook/importer.py
@@ -29,39 +29,20 @@ class Importer():
         
         return f'PG:host={hostname} port={portnum} user={username} dbname={database} password={password}'
 
-    def get_latest_version(self, schema_name):
-        src_connection = create_engine(self.src_engine_url).connect()
-        query = f'SELECT table_name\
-                FROM information_schema.tables AS t\
-                WHERE t.table_schema=\'{schema_name}\''
-
-        result = src_connection.execute(query)
-
-        def try_date(date): 
-            try: 
-                return parser.parse(date, dayfirst=False)
-            except: 
-                return None
-
-        date_times = [i for i in [(try_date(date[0]), date[0]) for date in result] if i[0] is not None]
-        if len(date_times) == 0: 
-            return 'latest'
-        else: 
-            return max(date_times, key=itemgetter(0))[1]
-
     def import_table(self, schema_name, version='latest'):
-        if version == 'latest': 
-            version = self.get_latest_version(schema_name)
-            
         srcDS = gdal.OpenEx(self.src_engine, gdal.OF_VECTOR)
         dstDS = gdal.OpenEx(self.dst_engine, gdal.OF_VECTOR)
 
-        print(f'Importing {schema_name} to build...\n')
+        print(f'Importing {schema_name}."{version}" to build...\n')
 
-        gdal.VectorTranslate(
-            dstDS,
-            srcDS,
-            format='PostgreSQL',
-            layerName=f'{schema_name}."{version}"',
-            accessMode='overwrite',
-            callback=gdal.TermProgress)
+        try: 
+            gdal.VectorTranslate(
+                dstDS,
+                srcDS,
+                SQLStatement=f'SELECT * FROM {schema_name}."{version}"',
+                format='PostgreSQL',
+                layerName=schema_name,
+                accessMode='overwrite',
+                callback=gdal.TermProgress)
+        except: 
+            print(f'Importing {schema_name}."{version}" failed...\n')
